6a9ad0b933c26e112c119fe11b42517428fc8db2
Alan Cesar Elias <alancesarelias@gmail.com>
2025-11-02 22:32:38 -0300

    feat: introduce add feed api

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"rss-summary/handler"
 	"rss-summary/internal/database"
+	"rss-summary/internal/feed"
 	"rss-summary/usecase"
 
 	"github.com/go-chi/chi/v5"
@@ -27,10 +28,15 @@ func main() {
 
 	sqliteDatabase := database.NewGorm(db)
 	readUseCase := usecase.NewRead(sqliteDatabase)
+	addUseCase := usecase.NewAddFeed(sqliteDatabase, feed.NewGoFeed())
 
 	r.Route("/articles", func(r chi.Router) {
 		r.Get("/", handler.GetFromDate(readUseCase))
 		r.Get("/today", handler.ListToday(readUseCase))
+	})
+
+	r.Route("/feeds", func(r chi.Router) {
+		r.Post("/", handler.AddFeed(addUseCase))
 	})
 
 	if err := http.ListenAndServe(":3000", r); err != nil {
