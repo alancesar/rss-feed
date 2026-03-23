bf2d1ad7d45a2e3a54bb9771c343905651594f4e
Alan Cesar Elias <alancesarelias@gmail.com>
2026-03-22 22:08:56 -0300

    refactor: save feed before publish its articles

--- usecase/publish.go
+++ usecase/publish.go
@@ -13,15 +13,21 @@ type (
 		Fetch(context.Context, string) (event.Feed, error)
 	}
 
+	FeedStore interface {
+		SaveFeed(context.Context, rss.Feed) error
+	}
+
 	PublishFeed struct {
-		publisher Publisher
 		fetcher   FeedFetcher
+		store     FeedStore
+		publisher Publisher
 	}
 )
 
-func NewPublishFeed(publisher Publisher, fetcher FeedFetcher) *PublishFeed {
+func NewPublishFeed(fetcher FeedFetcher, store FeedStore, publisher Publisher) *PublishFeed {
 	return &PublishFeed{
 		publisher: publisher,
+		store:     store,
 		fetcher:   fetcher,
 	}
 }
@@ -30,21 +36,28 @@ func (uc PublishFeed) Execute(ctx context.Context, url string) (rss.Feed, error)
 	log := zerolog.Ctx(ctx)
 
 	log.Info().Str("url", url).Msg("fetching feed")
-	feed, err := uc.fetcher.Fetch(ctx, url)
+	fetchedFeed, err := uc.fetcher.Fetch(ctx, url)
 	if err != nil {
 		return rss.Feed{}, err
 	}
 
-	log.Info().Str("feed", feed.Name).Int("articles", len(feed.Articles)).Msg("publishing feed.found event")
+	feed := rss.Feed{
+		ID:   fetchedFeed.FeedID,
+		Name: fetchedFeed.Name,
+		URL:  fetchedFeed.URL,
+	}
+
+	log.Info().Str("feed", fetchedFeed.Name).Msg("saving feed")
+	if err := uc.store.SaveFeed(ctx, feed); err != nil {
+		return rss.Feed{}, err
+	}
+
+	log.Info().Str("feed", fetchedFeed.Name).Int("articles", len(fetchedFeed.Articles)).Msg("publishing feed.found event")
 	if err := uc.publisher.Publish(ctx, "feed.found", event.Event{
-		Payload: feed,
+		Payload: fetchedFeed,
 	}); err != nil {
 		return rss.Feed{}, err
 	}
 
-	return rss.Feed{
-		ID:   feed.FeedID,
-		Name: feed.Name,
-		URL:  feed.URL,
-	}, nil
+	return feed, nil
 }
